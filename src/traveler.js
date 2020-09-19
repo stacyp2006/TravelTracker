5d5ab054290c7ba052dd20e32ccb27bbcd48b29e
Stacy Potten <stacylynnaz@gmail.com>
2020-09-19 11:45:44 -0600

    Add test and method to get all trips for a user

--- src/traveler.js
+++ src/traveler.js
@@ -3,11 +3,25 @@ class Traveler {
     this.id = travelerData.id;
     this.name = travelerData.name;
     this.travelerType = travelerData.travelerType;
+    this.allTrips = [];
     this.pastTrips = [];
     this.currentTrip = {};
     this.upcomingTrips = [];
     this.pendingTrips = [];
   }
+
+  findAllTrips(tripData) {
+    let travelerTrips = tripData.filter(trip => {
+      return this.id === trip.userID
+    })
+    this.allTrips = travelerTrips;
+  }
+  // addPastTrips(tripData) {
+  //   let oldTrips = tripData.filter(trip => {
+  //     return Date.parse(trip.date) <= Date.now()
+  //   })
+  //   this.pastTrips.push(oldTrips);
+  // }
 }
 
 
--- test/traveler-test.js
+++ test/traveler-test.js
@@ -9,7 +9,7 @@ import tripData from '../src/data/trip-data.js';
 describe('Traveler', () => {
   let traveler1;
   beforeEach(() => {
-    traveler1 = new Traveler(travelerData[0], tripData);
+    traveler1 = new Traveler(travelerData[47], tripData);
   })
 
   it('should be a function', () => {
@@ -17,26 +17,89 @@ describe('Traveler', () => {
   })
 
   it('should take in data from travelerData', () => {
-    expect(traveler1.id).to.equal(1);
-    expect(traveler1.name).to.equal("Ham Leadbeater");
-    expect(traveler1.travelerType).to.equal("relaxer");
+    expect(traveler1.id).to.equal(48);
+    expect(traveler1.name).to.equal("Lurlene Raisbeck");
+    expect(traveler1.travelerType).to.equal("photographer");
   })
 
-  it.only('should have a default value for past trips', () => {
+  it('should have a default value for past trips', () => {
     expect(traveler1.pastTrips).to.deep.equal([]);
   })
 
-  it.only('should have a default value for current trip', () => {
+  it('should have a default value for current trip', () => {
     expect(traveler1.currentTrip).to.deep.equal({});
   })
 
-  it.only('should have a default value for upcoming trips', () => {
+  it('should have a default value for upcoming trips', () => {
     expect(traveler1.upcomingTrips).to.deep.equal([]);
   })
 
-  it.only('should have a default value for pending trips', () => {
+  it('should have a default value for pending trips', () => {
     expect(traveler1.pendingTrips).to.deep.equal([]);
   })
+
+  it('should have a default value for all trips', () => {
+    expect(traveler1.allTrips).to.deep.equal([]);
+  })
+
+  it('should return all trips for a user', () => {
+    traveler1.findAllTrips(tripData)
+    expect(traveler1.allTrips.length).to.equal(7)
+  })
+
+  // it('should add trips to the pastTrips array after they occur', () => {
+  //   traveler1.addPastTrips(tripData);
+  //   expect(traveler1.pastTrips).to.deep.equal([{
+  //     id: 28,
+  //     userID: 48,
+  //     destinationID: 31,
+  //     travelers: 3,
+  //     date: "2020/02/03",
+  //     duration: 14,
+  //     status: "approved",
+  //     suggestedActivities: []
+  //   },
+  //   {
+  //     id: 122,
+  //     userID: 48,
+  //     destinationID: 17,
+  //     travelers: 2,
+  //     date: "2020/09/06",
+  //     duration: 5,
+  //     status: "approved",
+  //     suggestedActivities: []
+  //   },
+  //   {
+  //     id: 133,
+  //     userID: 48,
+  //     destinationID: 46,
+  //     travelers: 5,
+  //     date: "2019/09/15",
+  //     duration: 10,
+  //     status: "approved",
+  //     suggestedActivities: []
+  //   },
+  //   {
+  //     id: 156,
+  //     userID: 48,
+  //     destinationID: 40,
+  //     travelers: 1,
+  //     date: "2019/08/02",
+  //     duration: 7,
+  //     status: "approved",
+  //     suggestedActivities: []
+  //   },
+  //   {
+  //     id: 162,
+  //     userID: 48,
+  //     destinationID: 25,
+  //     travelers: 2,
+  //     date: "2020/06/22",
+  //     duration: 17,
+  //     status: "approved",
+  //     suggestedActivities: []
+  //   }, ])
+  //})
 
 
 })
