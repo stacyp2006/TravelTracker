08f5da979a16e11cf03ee6ff397cb394e19bc87a
Stacy Potten <stacylynnaz@gmail.com>
2020-09-18 20:13:53 -0600

    Add properties for various trip types with default values and tests for same.

--- src/traveler.js
+++ src/traveler.js
@@ -1,8 +1,12 @@
 class Traveler {
-  constructor(travelerData) {
+  constructor(travelerData, tripData) {
     this.id = travelerData.id;
     this.name = travelerData.name;
     this.travelerType = travelerData.travelerType;
+    this.pastTrips = [];
+    this.currentTrip = {};
+    this.upcomingTrips = [];
+    this.pendingTrips = [];
   }
 }
 
--- test/traveler-test.js
+++ test/traveler-test.js
@@ -9,30 +9,34 @@ import tripData from '../src/data/trip-data.js';
 describe('Traveler', () => {
   let traveler1;
   beforeEach(() => {
-    traveler1 = new Traveler(travelerData[0]);
+    traveler1 = new Traveler(travelerData[0], tripData);
   })
 
   it('should be a function', () => {
     expect(Traveler).to.be.a('function');
   })
 
-  it.only('should take in data from travelerData', () => {
-    console.log(travelerData[0]);
+  it('should take in data from travelerData', () => {
     expect(traveler1.id).to.equal(1);
     expect(traveler1.name).to.equal("Ham Leadbeater");
     expect(traveler1.travelerType).to.equal("relaxer");
   })
 
-  it('should have past trips', () => {
+  it.only('should have a default value for past trips', () => {
     expect(traveler1.pastTrips).to.deep.equal([]);
   })
 
-  it('should have a current trip', () => {
+  it.only('should have a default value for current trip', () => {
     expect(traveler1.currentTrip).to.deep.equal({});
   })
 
-  it('should have upcoming trips', () => {
+  it.only('should have a default value for upcoming trips', () => {
     expect(traveler1.upcomingTrips).to.deep.equal([]);
   })
+
+  it.only('should have a default value for pending trips', () => {
+    expect(traveler1.pendingTrips).to.deep.equal([]);
+  })
+
 
 })
