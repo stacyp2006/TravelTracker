8cec5c5db928b0dcafe5a575ce84e37353e174c9
Stacy Potten <stacylynnaz@gmail.com>
2020-09-20 18:49:17 -0600

    Add fetches to get trip and traveler data

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -10,11 +10,15 @@ let domUpdates = {
 
   getTravelData: () => {
     Promise.all([
+      fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/48'),
+      fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips'),
       fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
     ])
       .then(responses => Promise.all(responses.map(response => response.json())))
       .then(responses => {
-        domUpdates.destinations = responses[0].destinations;
+        domUpdates.traveler = responses[0].travelers;
+        domUpdates.trip = responses[1].trip;
+        domUpdates.destinations = responses[2].destinations;
         domUpdates.getDestinations();
       })
       .catch(error => console.log(error))
@@ -23,7 +27,6 @@ let domUpdates = {
 
   getDestinations: () => {
     const destinationMenu = document.querySelector('.destination-menu');
-    // console.log("hello", domUpdates.destinations);
     if (domUpdates.destinations.length > 0) {
       domUpdates.destinations.forEach(destination => {
         destinationMenu.insertAdjacentHTML('beforeend', `<option value="${destination.id}">${destination.destination}</option>`)
